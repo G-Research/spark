e1091a74609e9ce8bc392330cf6d4f23f55f0f60
Enrico Minack <github@enrico.minack.dev>
2021-06-19 08:28:07 +0200

    Make public methods Java compatible

--- sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
@@ -1955,7 +1955,7 @@ class Dataset[T] private[sql](
   }
 
   def observe(observation: Observation, expr: Column, exprs: Column*): Dataset[T] = {
-    observation.on(this)(expr, exprs: _*)
+    observation.on(this, expr, exprs: _*)
   }
 
   /**
