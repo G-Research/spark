8c5d3bc20828b7fb20b3b29b0838c845fb023102
Enrico Minack <github@enrico.minack.dev>
2023-01-26 15:11:56 +0100

    Introduce ScopedExpression that resolves an expression against a scoped set of attributes only

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
@@ -286,6 +286,7 @@ class Analyzer(override val catalogManager: CatalogManager)
       ResolveFieldNameAndPosition ::
       AddMetadataColumns ::
       DeduplicateRelations ::
+      ResolveScopedExpression ::
       ResolveReferences ::
       ResolveLateralColumnAliasReference ::
       ResolveExpressionsWithNamePlaceholders ::
@@ -1593,34 +1594,12 @@ class Analyzer(override val catalogManager: CatalogManager)
           Generate(newG.asInstanceOf[Generator], join, outer, qualifier, output, child)
         }
 
-      case mg: MapGroups if mg.dataOrder.exists(!_.resolved) =>
-        // Resolve against `AppendColumns`'s children, instead of `AppendColumns`,
-        // because `AppendColumns`'s serializer might produce conflict attribute
-        // names leading to ambiguous references exception.
-        val planForResolve = mg.child match {
-          case appendColumns: AppendColumns => appendColumns.child
-          case plan => plan
-        }
-        val resolvedOrder = mg.dataOrder
-            .map(resolveExpressionByPlanOutput(_, planForResolve).asInstanceOf[SortOrder])
-        mg.copy(dataOrder = resolvedOrder)
-
       // Left and right sort expression have to be resolved against the respective child plan only
       case cg: CoGroup if cg.leftOrder.exists(!_.resolved) || cg.rightOrder.exists(!_.resolved) =>
-        // Resolve against `AppendColumns`'s children, instead of `AppendColumns`,
-        // because `AppendColumns`'s serializer might produce conflict attribute
-        // names leading to ambiguous references exception.
-        val (leftPlanForResolve, rightPlanForResolve) = Seq(cg.left, cg.right).map {
-          case appendColumns: AppendColumns => appendColumns.child
-          case plan => plan
-        } match {
-          case Seq(left, right) => (left, right)
-        }
-
         val resolvedLeftOrder = cg.leftOrder
-          .map(resolveExpressionByPlanOutput(_, leftPlanForResolve).asInstanceOf[SortOrder])
+          .map(resolveExpressionByPlanOutput(_, cg.left).asInstanceOf[SortOrder])
         val resolvedRightOrder = cg.rightOrder
-          .map(resolveExpressionByPlanOutput(_, rightPlanForResolve).asInstanceOf[SortOrder])
+          .map(resolveExpressionByPlanOutput(_, cg.right).asInstanceOf[SortOrder])
 
         cg.copy(leftOrder = resolvedLeftOrder, rightOrder = resolvedRightOrder)
 
@@ -3683,6 +3662,20 @@ class Analyzer(override val catalogManager: CatalogManager)
         // find common column names from both sides
         val joinNames = left.output.map(_.name).intersect(right.output.map(_.name))
         commonNaturalJoinProcessing(left, right, joinType, joinNames, condition, hint)
+    }
+  }
+
+  /**
+   * Resolves expressions against their scope of attributes.
+   */
+  object ResolveScopedExpression extends Rule[LogicalPlan] {
+    override def apply(plan: LogicalPlan): LogicalPlan = plan.resolveExpressions {
+      case se: ScopedExpression if se.resolved => se.child
+      case ScopedExpression(expr, attributes) =>
+        expr.transformDown {
+          case u@UnresolvedAttribute(nameParts) =>
+            attributes.resolve(nameParts, resolver).getOrElse(u)
+        }
     }
   }
 
