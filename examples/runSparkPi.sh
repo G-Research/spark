f3dd4046ebfdc120d7c0734870571742e8d627eb
George Jahad <george@georgejahad.com>
2025-01-31 15:14:57 -0800

    cleanup

--- examples/runSparkPi.sh
+++ examples/runSparkPi.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
+echo starting SparkPi driver
 armadactl submit examples/spark-pi-driver.yaml >& /tmp/jobid.txt
 export JOB_ID=`cat /tmp/jobid.txt | awk  '{print $5}'`
 cat /tmp/jobid.txt
-echo waiting for SparkPi driver to start: `date`
+echo waiting for SparkPi driver to start:
 sleep 20
 export IP_ADDR=`kubectl get pod "armada-$JOB_ID-0" -o jsonpath='{.status.podIP}'`
 echo driver has ip address: $IP_ADDR
